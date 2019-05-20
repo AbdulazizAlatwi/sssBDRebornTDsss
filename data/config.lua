a8c891304af498b3d6bbe1b4264617a69cfa79d3
AbdulazizAlatwi <47693808+AbdulazizAlatwi@users.noreply.github.com>
2019-05-20 11:33:18 +0300

    s

---
+++ data/config.lua
@@ -0,0 +1,23 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "groupmanager",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "fun"
+  },
+  info_text = "》BDReborn (TDBot Branch) V2\nAn advanced administration bot based on Beyond Reborn V6 updated for https://valtman.name/telegram-bot\n\n》https://github.com/BeyondTeam/BDReborn\n\n》Admins :\n》@SoLiD ➣ Founder & Developer《\n》@ToOfan ➣ Developer《\n》@Xamarin_Developer ➣ Developer《\n》 MAKAN ➣ Developer《\n》@SenatorHost ➣ Sponsor《\n\n》Special thanks to :\n》@kuncen\n》@Vysheng\n》@MrHalix\n》Beyond Team Members\n\n》Our channel :\n》@BeyondTeam《\n\n》Our website :\n》http://Beyond-Dev.iR\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    545985657,
+    545985657,
+    545985657
+  }
+}
+return _
+end
